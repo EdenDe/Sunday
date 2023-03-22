1166a3d7fb3e311e255785a8cf6edc97639cbffe
lior88844 <lior88844@gmail.com>
2023-03-22 16:23:46 +0200

    date

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,52 +1,39 @@
 <template>
   <DraggableNext v-if="currTasks" v-model="currTasks" @moved="changeIndex">
-    <section class="task-list" v-for="task, index in currTasks" :key="index">
+    <section class="task-list" v-for="(task, index) in currTasks" :key="index">
       <TaskPreview :task="task" />
     </section>
   </DraggableNext>
-  <form @submit.prevent="onAddTask" class="add-task-input-container">
-    <input placeholder="+ Add task" type="text" v-model="newTask" />
-  </form>
 </template>
 
 <script>
 import { VueDraggableNext } from "vue-draggable-next";
-import TaskPreview from './TaskPreview.vue';
-import { eventBus } from '../services/event-bus.service'
+import TaskPreview from "./TaskPreview.vue";
 
 export default {
-  name: 'TaskList',
-  props: { tasks: Array, groupId: String },
+  name: "TaskList",
+  props: { tasks: Array },
   data() {
     return {
-      newTask: '',
-      currTasks: []
-    }
+      currTasks: [],
+    };
   },
   methods: {
     log(event) {
       console.log(event);
       console.log("this.labels:", this.labels);
     },
-    changeIndex() {
-
-    },
-    onAddTask() {
-      eventBus.emit('addTask', {
-        groupId: this.groupId,
-        newTask: this.newTask
-      })
-    },
+    changeIndex() {},
   },
   created() {
     //console.log("TaskList", this.tasks)
-    this.currTasks = this.tasks
+    this.currTasks = this.tasks;
   },
   components: {
     TaskPreview,
     DraggableNext: VueDraggableNext,
   },
-}
+};
 </script>
 
 <style></style>
