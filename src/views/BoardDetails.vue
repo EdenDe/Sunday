d955a3531b54f8f25e9e0980003c3c347bc62b24
lior88844 <lior88844@gmail.com>
2023-03-22 22:42:15 +0200

    changes for workspace

--- src/views/BoardDetails.vue
+++ src/views/BoardDetails.vue
@@ -8,35 +8,17 @@
 </template>
 
 <script>
-import { Container, Draggable } from 'vue3-smooth-dnd'
-import BoardHeader from '../components/BoardHeader.vue'
-import GroupList from '../components/GroupList.vue'
-import ToolTip from '../components/Cmps/ToolTip.vue'
+import { Container, Draggable } from "vue3-smooth-dnd";
+import BoardHeader from "../components/BoardHeader.vue";
+import GroupList from "../components/GroupList.vue";
+import ToolTip from "../components/Cmps/ToolTip.vue";
 
 export default {
-  methods: {
-    onDrop(dropResult) {
-      this.groups = this.applyDrag(this.groups, dropResult)
-    },
-    applyDrag(arr, dragResult) {
-      const { removedIndex, addedIndex, payload } = dragResult
-      if (removedIndex === null && addedIndex === null) return arr
-      const result = [...arr]
-      let itemToAdd = payload
-
-      if (removedIndex !== null) {
-        itemToAdd = result.splice(removedIndex, 1)[0]
-      }
-      if (addedIndex !== null) {
-        result.splice(addedIndex, 0, itemToAdd)
-      }
-      return result
-    },
-  },
+  methods: {},
   computed: {
     groups() {
-      console.log(this.$store.getters.groups)
-      return this.$store.getters.groups
+      console.log(this.$store.getters.groups);
+      return this.$store.getters.groups;
     },
   },
   components: {
@@ -46,6 +28,5 @@ export default {
     Draggable,
     ToolTip,
   },
-}
+};
 </script>
-
