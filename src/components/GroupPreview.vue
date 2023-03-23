583961da63874c010cd8b4c4bd9d7e1175f86b0d
lior88844 <lior88844@gmail.com>
2023-03-23 22:14:07 +0200

    changes in the title

--- src/components/GroupPreview.vue
+++ src/components/GroupPreview.vue
@@ -1,25 +1,48 @@
 <template>
   <section class="group-preview">
-    <div class="grid-title">
+    <div class="grid-title flex align-items">
       <div class="svg-wrapper"></div>
-      <span contenteditable class="group-title" @focusout="onType($event.target.innerText)"
-        :style="{ color: group.color }">
-        {{ group.title }}
-      </span>
+      <div class="group-title-wrapper flex align-center">
+        <span
+          contenteditable
+          class="group-title"
+          @focusout="onType($event.target.innerText)"
+          :style="{ color: group.color }"
+        >
+          {{ group.title }}
+        </span>
+        <span class="tasks-num flex align-items justify-start"
+          >{{ group.tasks.length }} Tasks</span
+        >
+      </div>
     </div>
-    <span class="tasks-num">{{ group.tasks.length }} Tasks</span>
     <Container class="group-labels">
       <Draggable v-for="(label, index) in labels" :key="label">
-        <div v-if="index === 1" class="first-col-color" :style="{ backgroundColor: group.color }"></div>
-        {{ label === 'taskTitle' ? 'title' : label }}
+        <div
+          v-if="index === 1"
+          class="first-col-color"
+          :style="{ backgroundColor: group.color }"
+        ></div>
+        {{ label === "taskTitle" ? "title" : label }}
       </Draggable>
     </Container>
 
-    <TaskList :tasks="group.tasks" :groupBgColor="group.color" @updateProp="updateProp" />
+    <TaskList
+      :tasks="group.tasks"
+      :groupBgColor="group.color"
+      @updateProp="updateProp"
+    />
     <div class="add-task-container">
-      <div class="first-col-color" :style="{ backgroundColor: group.color }"></div>
+      <div
+        class="first-col-color"
+        :style="{ backgroundColor: group.color }"
+      ></div>
       <form @submit.prevent="onAddTask" class="add-task-input-container">
-        <input placeholder="+ Add task" type="text" v-model="newTask.taskTitle" />
+        <input
+          placeholder="+ Add task"
+          type="text"
+          v-model="newTask.taskTitle"
+        />
       </form>
     </div>
     <ProgressBar :tasks="group.tasks" />
@@ -27,12 +50,12 @@
 </template>
 
 <script>
-import TaskList from './TaskList.vue'
-import ProgressBar from './ProgressBar.vue'
+import TaskList from "./TaskList.vue";
+import ProgressBar from "./ProgressBar.vue";
 import { Container, Draggable } from "vue3-smooth-dnd";
 
 export default {
-  name: 'GroupPreview',
+  name: "GroupPreview",
   props: {
     group: Object,
   },
@@ -50,31 +73,31 @@ export default {
       //   'file',
       // ],
       newTask: {
-        taskTitle: '',
+        taskTitle: "",
       },
-    }
+    };
   },
   methods: {
     log(event) {
-      console.log(event)
+      console.log(event);
     },
     onType(txt) {
-      this.updateProp(null, 'title', txt)
+      this.updateProp(null, "title", txt);
     },
     updateProp(taskId, prop, toUpdate) {
       this.$store.dispatch({
-        type: 'updateCurrBoard',
+        type: "updateCurrBoard",
         groupId: this.group.id,
         taskId,
         prop,
         toUpdate,
-      })
+      });
     },
     onAddTask() {
-      let group = JSON.parse(JSON.stringify(this.group))
-      group.tasks.push({ ...this.newTask })
-      this.updateProp(null, 'tasks', group.tasks)
-      this.newTask.taskTitle = ''
+      let group = JSON.parse(JSON.stringify(this.group));
+      group.tasks.push({ ...this.newTask });
+      this.updateProp(null, "tasks", group.tasks);
+      this.newTask.taskTitle = "";
     },
     onDrop(dropResult) {
       this.items = this.applyDrag(this.items, dropResult);
@@ -82,46 +105,47 @@ export default {
   },
   computed: {
     labels() {
-      let labels = [null, null, null]
-      labels.push(...this.$store.getters.cmpOrder.slice(1))
-      return labels
+      let labels = [null, null, null];
+      labels.push(...this.$store.getters.cmpOrder.slice(1));
+      return labels;
     },
     groupStatusProgress() {
       let res = this.group.tasks.reduce((obj, { status }) => {
-        if (!obj[status]) obj[status] = 0
-        obj[status] += 1
-        return obj
-      }, {})
+        if (!obj[status]) obj[status] = 0;
+        obj[status] += 1;
+        return obj;
+      }, {});
 
-      let statusLabel = this.$store.getters.statusLabels
-      let totalTaskLength = this.group.tasks.length
+      let statusLabel = this.$store.getters.statusLabels;
+      let totalTaskLength = this.group.tasks.length;
 
       statusLabel.map(({ title, color }) => {
         if (res[title]) {
-          let presentageWidth = (res[title] / totalTaskLength) * 100
+          let presentageWidth = (res[title] / totalTaskLength) * 100;
 
           res[title] = {
-            width: Math.round(presentageWidth) + '%',
+            width: Math.round(presentageWidth) + "%",
             color: color,
-            title: `${title} ${res[title]
-              }/${totalTaskLength} ${presentageWidth.toFixed(1)}%`,
-          }
+            title: `${title} ${
+              res[title]
+            }/${totalTaskLength} ${presentageWidth.toFixed(1)}%`,
+          };
         }
-      })
+      });
 
-      return res
+      return res;
     },
   },
   watch: {
     group() {
-      console.log(this.group)
+      console.log(this.group);
     },
   },
   components: {
     TaskList,
     ProgressBar,
     Container,
-    Draggable
+    Draggable,
   },
-}
+};
 </script>
