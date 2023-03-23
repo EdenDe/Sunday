20add442715fa3635fda26e99a51f34125406c59
lior88844 <lior88844@gmail.com>
2023-03-23 15:25:43 +0200

    changed components names

--- src/components/TaskPreview.vue
+++ src/components/TaskPreview.vue
@@ -11,12 +11,12 @@
 
 <script>
 import checkbox from "./dynamicCmps/Checkbox.vue";
-import date from "./dynamicCmps/Date.vue";
+import date from "./dynamicCmps/DatePicker.vue";
 import timeline from "./dynamicCmps/TimelinePicker.vue";
-import person from "./dynamicCmps/Person.vue";
-import priority from "./dynamicCmps/Priority.vue";
-import status from "./dynamicCmps/Status.vue";
-import TaskTitle from "./dynamicCmps/Title.vue";
+import person from "./dynamicCmps/PersonPicker.vue";
+import priority from "./dynamicCmps/PriorityPicker.vue";
+import status from "./dynamicCmps/StatusPicker.vue";
+import TaskTitle from "./dynamicCmps/TitleInput.vue";
 
 export default {
   name: "TaskPreview",
