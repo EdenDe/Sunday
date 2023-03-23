4e97e835ee4161504627f2920090244b1d9d67d2
TamarShalem93 <stamar46@gmail.com>
2023-03-23 10:04:21 +0200

    labelPicer works!!

--- src/components/DynamicCmps/Status.vue
+++ src/components/DynamicCmps/Status.vue
@@ -4,21 +4,20 @@
     class="status flex justify-center align-center"
     @click="toggleColorPicker"
   >
-    {{ status.taskTitle }}
-    <div v-if="isPickerOpen" class="color-picker-container">
-      <div
-        v-for="status in statusList"
-        :style="{ backgroundColor: status.color }"
-      >
-        {{ status.taskTitle }}
-      </div>
-    </div>
+    {{ status.title }}
+
+    <LabelPicker
+      v-if="isPickerOpen"
+      :labels="statusList"
+      @setLabel="setLabel"
+    ></LabelPicker>
   </div>
 </template>
 
 <script>
+import LabelPicker from '../LabelPicker.vue'
 export default {
-  name: 'TaskStatus',
+  name: 'TaskStatus ',
   props: {
     info: String,
   },
@@ -30,29 +29,28 @@ export default {
     }
   },
   created() {
-    this.labels = this.$store.getters.statusLabels
-    this.status = this.labels.find((label) => label.taskTitle === this.info)
+    let labels = this.$store.getters.statusLabels
+    this.status = labels.find((label) => label.title === this.info)
     if (!this.status) {
-      this.status = {
-        taskTitle: '',
-        color: '#c4c4c4',
-      }
+      this.status = labels[5]
     }
-    console.log(this.labels)
   },
   methods: {
     toggleColorPicker() {
       this.isPickerOpen = !this.isPickerOpen
     },
-    setLabel(label) {
-      console.log(label)
+    setLabel(labelTitle) {
       this.toggleColorPicker
+      this.$emit('updateProp', 'status', labelTitle)
     },
   },
   computed: {
     statusList() {
       return this.$store.getters.statusLabels
     },
+  },
+  components: {
+    LabelPicker,
   },
 }
 </script>
