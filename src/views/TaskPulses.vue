ab00d08adb43cbfbb73ab74bb14df30ac9696b03
Eden <derhyeden@gmail.com>
2023-03-24 17:17:10 +0300

    task pulses added

--- src/views/TaskPulses.vue
+++ src/views/TaskPulses.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="task-pulse">
-    <button>X</button>
-    <TaskTitle />
+    <button class="btn-close" v-icon="'closePulse'">X</button>
+    <TaskTitle :info="task.taskTitle" />
     <img src="https://cdn.monday.com/images/pulse-page-empty-state.svg" />
   </section>
 </template>
@@ -10,14 +10,32 @@
 export default {
   data() {
     return {
-
+      task: null
     }
   },
   methods: {
 
   },
-  computed: {
+  watch: {
+    taskId: {
+      handler() {
+        debugger
+        const groups = this.$store.getters.groups
+        const group = groups.find(g => g.id === this.$route.params.groupId)
+        const task = group.find(t => t.id === this.taskId)
+        if (!task) {
 
+          this.$router.back()
+          return
+        }
+      },
+      immediate: true
+    }
+  },
+  computed: {
+    taskId() {
+      return this.$route.params.taskId
+    }
   },
   created() {
     console.log('pusles', this.$route)
