cfc790e5a2fd4a827a4150094e7e824135a6eae9
Eden <derhyeden@gmail.com>
2023-03-25 21:12:56 +0300

    color picker tamplate added

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="color-picker">
+    <div></div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ColorPicker',
+  data() {
+    return {
+      colors: ['#037f4c', '#00c875', '#9cd326', '#cab641', '#ffcb00', '#784bd1',
+        '#a25ddc', '#a25ddc', '#66ccff', '#bb3354', '#e2445c', '#ff5ac4', '#ff642e',
+        '#fdab3d', '#fdab3d', '#fdab3d', '#808080'
+      ]
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  created() {
+
+  },
+  components: {
+
+  },
+}
+</script>
+
+<style></style>
