0c31d57422694645b155d21531d060a5ca49adca
Eden <derhyeden@gmail.com>
2023-03-25 13:39:10 +0300

    designed task pulse

---
+++ src/components/UpdateLog.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="update-log grid">
+    <textarea @focus="changeSize" :rows="rowsNum" class="textarea-update-log" v-model="content"
+      placeholder="Write an update..." v-clickOutside="onClickOut"></textarea>
+    <button class="btn-update">Update</button>
+  </section>
+</template>
+
+<script>
+//import { VueEditor } from "vue2-editor";
+
+export default {
+  name: 'UpdateLog',
+  data() {
+    return {
+      content: '',
+      rowsNum: 1,
+      // customToolbar: [
+      //   ["bold", "italic", "underline"],
+      //   [{ list: "ordered" }, { list: "bullet" }],
+      //   ["image", "code-block"]
+      // ]
+    }
+  },
+  methods: {
+    changeSize() {
+      this.rowsNum = 5
+    },
+    onClickOut() {
+      if (this.content === '') {
+        this.rowsNum = 1
+      }
+    }
+  },
+  computed: {
+
+  },
+  created() {
+
+  },
+  components: {
+    //VueEditor
+  },
+}
+</script>
+
+<style></style>
--- src/views/TaskPulse.vue
+++ src/views/TaskPulse.vue
@@ -1,43 +1,78 @@
 <template>
   <section class="task-pulse">
-    <button class="btn-close" v-icon="'closePulse'">X</button>
-    <input v-model="task.taskTitle" />
-    <img src="https://cdn.monday.com/images/pulse-page-empty-state.svg" />
+    <header class="grid">
+      <button class="btn btn-close" v-icon="'closePulse'" @click="onBack">X</button>
+      <div>
+        <span contenteditable v-if="task" class="task-title"> {{ task.taskTitle }} </span>
+      </div>
+      <div class="tabs-wrapper flex align-center">
+        <div class="btn-tab-wrapper" :class="{ 'active-tab': activeTab === 'updateLog' }">
+          <button class="btn btn-tab" @click="activeTab = 'updateLog'">
+            <i class="task-update-icon" v-icon="'homeHeader'"></i>
+            Updates
+          </button>
+        </div>
+        <div class="border"></div>
+        <div class="btn-tab-wrapper" :class="{ 'active-tab': activeTab === 'activityLog' }"
+          @click="activeTab = 'activityLog'">
+          <button class="btn btn-tab">Activity Log</button>
+        </div>
+      </div>
+    </header>
+    <main class="content-wrapper">
+      <component :is="activeTab" />
+    </main>
+
+    <!-- <img src="https://cdn.monday.com/images/pulse-page-empty-state.svg" /> -->
   </section>
 </template>
 
 <script>
-import TitleInput from '../components/dynamicCmps/TitleInput.vue'
+import UpdateLog from '../components/UpdateLog.vue'
+import ActivityLog from '../components/ActivityLog.vue'
 
 export default {
   data() {
     return {
-      task: null
+      task: null,
+      activeTab: 'updateLog'
     }
   },
   methods: {
+    getTask() {
+      const groups = this.$store.getters.groups
+      if (groups) {
+        let task
+        groups.forEach(group => {
+          task = group.tasks.find(t => t.id === this.taskId)
 
+          if (task) {
+            this.task = task
+          }
+        })
+      }
+    },
+    onBack() {
+      this.$router.back()
+    }
   },
   computed: {
     taskId() {
       return this.$route.params.taskId
     }
   },
-  async created() {
-    const groups = await this.$store.getters.groups
-    debugger
-    let task
-    groups.forEach(group => {
-      task = group.tasks.find(t => t.id === this.taskId)
-
-      if (task) {
-        this.task = task
-      }
-    })
+  created() {
+    this.getTask()
+  },
+  watch: {
+    taskId() {
+      this.getTask()
+    }
   },
   components: {
-    TitleInput
-  },
+    UpdateLog,
+    ActivityLog
+  }
 }
 </script>
 
