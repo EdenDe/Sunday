e3d1cc8d22444e0d162cf5c2508ce2411ba7d2ef
lior88844 <lior88844@gmail.com>
2023-03-25 11:04:05 +0300

    sticky

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -30,91 +30,91 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import { utilService } from "../services/util.service";
+import { mapGetters } from 'vuex'
+import { utilService } from '../services/util.service'
 export default {
-  name: "ProgressBar",
+  name: 'ProgressBar',
   props: {
     tasks: Array,
   },
   data() {
     return {
       progressTimeline: null,
-    };
+    }
   },
   methods: {
     setProgressTimeline() {
       let timeline = this.tasks.reduce(
         (acc, task) => {
-          if (task["timeline"].length > 0) {
-            let timestamps = [...task["timeline"]];
-            let minTimestamp = Math.min(...timestamps);
-            let maxTimestamp = Math.max(...timestamps);
+          if (task['timeline'].length > 0) {
+            let timestamps = [...task['timeline']]
+            let minTimestamp = Math.min(...timestamps)
+            let maxTimestamp = Math.max(...timestamps)
             if (minTimestamp < acc[0]) {
-              acc[0] = minTimestamp;
+              acc[0] = minTimestamp
             }
             if (maxTimestamp > acc[1]) {
-              acc[1] = maxTimestamp;
+              acc[1] = maxTimestamp
             }
           }
-          return acc;
+          return acc
         },
         [Infinity, -Infinity]
-      );
+      )
       if (
         !timeline ||
         !timeline.length ||
         timeline[0] === Infinity ||
         timeline[1] === -Infinity
       ) {
-        this.progressTimeline = null;
-        return;
+        this.progressTimeline = null
+        return
       }
-      console.log(timeline);
-      this.progressTimeline = timeline;
+
+      this.progressTimeline = timeline
     },
   },
   computed: {
-    ...mapGetters(["cmpOrder", "statusLabels", "priorityLabels"]),
+    ...mapGetters(['cmpOrder', 'statusLabels', 'priorityLabels']),
     groupStatusProgress() {
       return (item) => {
         let res = this.tasks.reduce((obj, task) => {
-          if (!obj[task[item]]) obj[task[item]] = 0;
-          obj[task[item]] += 1;
-          return obj;
-        }, {});
+          if (!obj[task[item]]) obj[task[item]] = 0
+          obj[task[item]] += 1
+          return obj
+        }, {})
 
         let labels =
-          item === "status" ? [...this.statusLabels] : [...this.priorityLabels];
+          item === 'status' ? [...this.statusLabels] : [...this.priorityLabels]
 
-        let totalTaskLength = this.tasks.length;
+        let totalTaskLength = this.tasks.length
 
         labels.forEach(({ title, color }) => {
           if (res[title]) {
-            let presentageWidth = (res[title] / totalTaskLength) * 100;
+            let presentageWidth = (res[title] / totalTaskLength) * 100
 
             res[title] = {
-              width: Math.round(presentageWidth) + "%",
+              width: Math.round(presentageWidth) + '%',
               color: color,
               title: `${title} ${
                 res[title]
               }/${totalTaskLength} ${presentageWidth.toFixed(1)}%`,
-            };
+            }
           }
-        });
-        return res;
-      };
+        })
+        return res
+      }
     },
     groupTimelineProgressDates() {
-      this.setProgressTimeline();
-      if (!this.progressTimeline) return "-";
-      return utilService.formatDateRange(this.progressTimeline);
+      this.setProgressTimeline()
+      if (!this.progressTimeline) return '-'
+      return utilService.formatDateRange(this.progressTimeline)
     },
     groupTimelineProgressRange() {
-      this.setProgressTimeline();
-      if (!this.progressTimeline) return "-";
-      return utilService.getDaysBetween(this.progressTimeline);
+      this.setProgressTimeline()
+      if (!this.progressTimeline) return '-'
+      return utilService.getDaysBetween(this.progressTimeline)
     },
   },
-};
+}
 </script>
