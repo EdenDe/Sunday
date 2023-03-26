20d7da3165133b9e7ef6d3f5481cd9028c455578
TamarShalem93 <stamar46@gmail.com>
2023-03-26 20:18:13 +0300

    txt input

--- src/components/DynamicCmps/TxtInput.vue
+++ src/components/DynamicCmps/TxtInput.vue
@@ -1,6 +1,5 @@
 <template>
   <section class="task-txt">
-<<<<<<< HEAD
     <span
       contenteditable
       @focusin="onFocusIn"
@@ -8,10 +7,6 @@
       :style="{ fontSize: '14px' }"
       >{{ txt }}</span
     >
-=======
-    <span contenteditable @focusin="onFocusIn" @focusout="onFocusout($event.target.innerText)"
-      :style="{ fontSize: '14px' }">{{ txt }}</span>
->>>>>>> a24eaed20f29e8e9a45a648cedf4f5600f0de34f
   </section>
 </template>
 
